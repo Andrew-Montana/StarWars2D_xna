080910b55d5775b643529146a2f9a80b44233ace
Andrew-Montana <le09@ukr.net>
2017-10-15 11:33:55 +0300

    filled methods

--- 2D StarWars Fighter/2D StarWars Fighter/enemies/Imperial.cs
+++ 2D StarWars Fighter/2D StarWars Fighter/enemies/Imperial.cs
@@ -14,13 +14,24 @@ namespace _2D_StarWars_Fighter.enemies
     {
         public Texture2D texture, stand1, stand2, kneel1, kneel2;
         public Vector2 position;
-        public bool isVisible, isAttack, isMove, isPlayerDetected;
+        public bool isVisible, isAttack, isMove, isPlayerDetected, isPlayerConfines;
         public Rectangle boundingBox;
         public SpriteEffects spriteEffect; // looking left is - None ( custom )
-        public int confines; // confines of imperial movement. 8000-10000 on X for example
+     //   public int confines1,// confines2; // confines of imperial movement. 8000-10000 on X for example
+        public Player playerRef;
 
-        public Imperial()
+        public Imperial(Texture2D stand1, Texture2D stand2, Texture2D kneel1, Texture2D kneel2, Vector2 newPosition, Player player)
         {
+            this.stand1 = stand1;
+            this.stand2 = stand2;
+            this.kneel1 = kneel1;
+            this.kneel2 = kneel2;
+            texture = this.stand1;
+            position = newPosition;
+            playerRef = player;
+            spriteEffect = SpriteEffects.None;
+        //    confines1 = 8000;
+          //  confines2 = 10000;
 
         }
 
@@ -29,31 +40,68 @@ namespace _2D_StarWars_Fighter.enemies
             PlayerDetection();
             CheckDistance();
             Movement();
-            Rotate();
+          //  Rotate();
             Attack();
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-
+            spriteBatch.Draw(texture, position, null, Color.White, 0.0F, new Vector2(0, 0), 1F, spriteEffect, 0);
         }
 
         // #
 
         private void PlayerDetection()
         {
-
+            if (playerRef.position.X > 8000)
+            {
+                // если игрок заходит в границы 
+                isPlayerConfines = true;
+            }
+            else if (playerRef.position.X < 8000)
+            {
+                // если игрок вне границ
+                isPlayerConfines = false;
+            }
         }
 
         private void CheckDistance()
         {
+            if (isPlayerConfines)
+            {
+                // если позиция игрока меньше чем позиция объекта
+                if (playerRef.position.X < position.X)
+                {
+                    int result = (int)position.X - (int)playerRef.position.X;
+                    if (result <= 750)
+                    {
+                        isPlayerDetected = true;
+                        spriteEffect = SpriteEffects.None;
+                    }
+                }// если позиция игрока по X больше, чем у объекта
+                else if (playerRef.position.X > position.X)
+                {
+                    int result = (int)playerRef.position.X - (int)position.X;
+                    if (result <= 750)
+                    {
+                        isPlayerDetected = true;
+                        spriteEffect = SpriteEffects.FlipHorizontally;
+                    }
+                }
 
+            }
+            else if (isPlayerConfines == false)
+            {
+                isPlayerDetected = false;
+            }
         }
 
         private void Movement()
         {
-            // custom moving
-            // ifDetected player
+            if (isPlayerDetected)
+            {
+
+            }
         }
 
         private void Attack()
@@ -61,10 +109,6 @@ namespace _2D_StarWars_Fighter.enemies
 
         }
 
-        private void Rotate()
-        {
-
-        }
 
     }
 }
