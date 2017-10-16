2d39a71d5183d9028846b7c184acdd6d371479d8
Andrew-Montana <le09@ukr.net>
2017-10-16 15:36:23 +0300

    Structure for Boss of 3 Level

---
+++ 2D StarWars Fighter/2D StarWars Fighter/enemies/Boss3Level.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Media;
+
+namespace _2D_StarWars_Fighter.enemies
+{
+    class Boss3Level
+    {
+        public Texture2D texture;
+        public Vector2 position;
+
+        public Boss3Level()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+
+        // Lightsaber
+
+        private void StickOut()
+        {
+
+        }
+
+        private void Hide()
+        {
+
+        }
+
+        private void Throw()
+        {
+
+        }
+
+        // Force
+
+        private void Push()
+        {
+
+        }
+
+        private void Teleport()
+        {
+
+        }
+
+        // Death
+
+        private void Death()
+        {
+
+        }
+        
+        // Intro
+
+        private void EnterArea()
+        {
+
+        }
+
+
+    }
+}
